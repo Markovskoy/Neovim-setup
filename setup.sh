ea125da9753fc34e1e94158ffc3cd18560e17e55
Markovskoy <vikitiki913@gmail.com>
2025-07-28 23:10:36 +0000

    fix sh

--- setup.sh
+++ setup.sh
@@ -11,9 +11,20 @@ chmod u+x nvim.appimage
 ln -sf ~/.local/bin/squashfs-root/AppRun ~/.local/bin/nvim
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 
+
+echo "📦 Установка LazyGit..."
+cd /tmp
+curl -L -o lazygit.tar.gz https://github.com/jesseduffield/lazygit/releases/download/v0.53.0/lazygit_0.53.0_Linux_x86_64.tar.gz
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm -f lazygit.tar.gz lazygit
+echo "✅ LazyGit установлен. Версия:"
+lazygit --version
+
+
 echo "📁 Копируем конфиг Neovim из локальной папки"
 mkdir -p ~/.config/nvim/lazy
 cp ./nvim-config/init.lua ~/.config/nvim/init.lua
 cp -r ./nvim-config/lazy/lazy.nvim ~/.config/nvim/lazy/lazy.nvim
 
-echo "✅ Установка завершена. Запусти nvim и в нём команду :Lazy sync"
+echo "✅ Установка завершена. Запусти nvim"
